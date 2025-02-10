27fb54c5f88eaac1206cae9287bcd0a098a02954
Semini-Sawbhagya <161110096+Semini-Sawbhagya@users.noreply.github.com>
2025-02-10 16:32:06 +0530

    made changes

---
+++ frontend/src/components/Student/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './home.css'
+import Header from '../../Student/Header/header'
+//import ExploreMenu from '../../components/ExploreMenu/exploreMenu'
+import { useState } from 'react'
+//import FoodDisplay from '../../components/FoodDisplay/foodDisplay'
+//import AppDownload from '../../components/AppDownload/AppDownload'
+const home = () => {
+  
+  return (
+    <div>
+        <Header/>
+       
+    </div>
+  )
+}
+
+export default home
---
+++ frontend/src/components/Student/NavBar/NavBar.jsx
@@ -0,0 +1,30 @@
+import React,{useState} from 'react'
+import './NavBar.css'
+import logo from '../../../assets/logo.png'
+import { Link } from 'react-router-dom'
+import search_icon from '../../../assets/search_icon.png'
+const Navbar = () => {
+
+const [menu,setMenu] = useState("home");
+
+  return (
+    <div className='navbar'>
+      <Link to='/'><img src={logo} alt="" className="logo"/></Link>
+      <ul className="navbar-menu">
+      <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
+      <a href='#explore-menu' onClick={()=>setMenu("menu")}className={menu==="menu"?"active":""}>menu</a>
+      <a href='#app-download' onClick={()=>setMenu("mobile-app")}className={menu==="mobile-app"?"active":""}>mobile-app</a>
+      <a href='#footer' onClick={()=>setMenu("contact-us")}className={menu==="contact-us"?"active":""}>contact us</a>
+      </ul>
+      <div className="navbar-right">
+        <img src={search_icon} alt="" />
+        <div className='navbar-search-icon'>
+          
+        </div>
+        <button>sign in</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
