7497540dadaf7432973efa4affc27c001ce21b7c
Semini-Sawbhagya <161110096+Semini-Sawbhagya@users.noreply.github.com>
2025-02-10 17:19:32 +0530

    modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import SignUp from './pages/SignUp/SignUp'
+import React from 'react'
+import NavBar from './components/Student/NavBar/NavBar'
+import Home from './components/Student/Home/Home'
+import Footer from './components/Footer/Footer'
 
 function App() {
   
 
   return (
     <BrowserRouter>
+     <NavBar/>
     <Routes>
       <Route path="/" element={<SignUp />} />
+      <Route path='/home' element={<Home/>}/>
       
     </Routes>
+    <Footer/>
     </BrowserRouter>
+    
   )
 }
 
