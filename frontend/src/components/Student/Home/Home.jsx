706ccc0f1951cce8f94385d122a40fc3679eeb81
NS <157737507+NipunSachintha@users.noreply.github.com>
2025-02-12 23:59:55 +0530

    update home.jsx

--- frontend/src/components/Student/Home/Home.jsx
+++ frontend/src/components/Student/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './home.css'
 import Header from '../../Student/Header/header'
 //import ExploreMenu from '../../components/ExploreMenu/exploreMenu'
@@ -6,20 +6,34 @@ import { useState } from 'react'
 //import FoodDisplay from '../../components/FoodDisplay/foodDisplay'
 //import AppDownload from '../../components/AppDownload/AppDownload'
 import { useNavigate } from 'react-router-dom'
+import NavBar from '../NavBar/NavBar'
+
 
 const home = () => {
 
   const navigate = useNavigate();
+  const [messages, setMessages] = useState("");
 
   const handlelogout = () => {
     localStorage.removeItem("token");
     navigate("/login");
   }
-  
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/protected')
+      .then((response) => setMessages(response.data.message))
+      .catch(() =>{
+        localStorage.removeItem("token");
+        navigate("/login");
+      })
+  }, [navigate]);
+
   return (
     <div>
+        <NavBar/>
         <Header/>
         <button onClick={handlelogout}>Logout</button>
+
        
     </div>
   )
