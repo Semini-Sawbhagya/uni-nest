622e47e451e292776b11965b5541b5664ef5c5ef
NS <157737507+NipunSachintha@users.noreply.github.com>
2025-02-16 14:50:20 +0530

    added a home page for landlords

---
+++ frontend/src/components/LandLord/LandlordHome.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+
+const LandLordHome = () => {
+
+  
+  return (
+    <div>
+        <h1>LandLord Home</h1>
+    </div>
+  )
+}
+
+export default LandLordHome
