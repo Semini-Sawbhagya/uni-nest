06060efcc46fa60a2f98f9c600915061047c8e3c
NS <157737507+NipunSachintha@users.noreply.github.com>
2025-03-03 22:03:34 +0530

    modified landlord nav bar

--- frontend/src/components/LandLord/LandlordHome.jsx
+++ frontend/src/components/LandLord/LandlordHome.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './LandlordHome.css';
 import Footer from '../Footer/Footer';
-import Navbar from '../Student/NavBar/NavBar';
+import Navbar from '../LandLord/NavBar/NavBar'
 import Cookies from 'js-cookie';
 import { jwtDecode } from 'jwt-decode';
 import axios from 'axios';
